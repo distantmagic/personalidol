8aa8ae5523b25110c58c9fda48345a7a007afe2c
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2019-01-28 10:40:02 +0100

    adjust bus clock

--- src/framework/classes/BusClock.js
+++ src/framework/classes/BusClock.js
@@ -10,7 +10,7 @@ import type { BusClockTick as BusClockTickInterface } from "../interfaces/BusClo
 export default class BusClock implements BusClockInterface {
   delay: number;
 
-  constructor(delay: number = 10) {
+  constructor(delay: number = 40) {
     this.delay = delay;
   }
 
