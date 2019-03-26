75615800cb1c12e495ce5e707623322e1ccecd5b
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2019-03-27 00:30:22 +0100

    update character sheet

--- src/components/HudModalCharacter.js
+++ src/components/HudModalCharacter.js
@@ -32,116 +32,72 @@ export default class HudModalRouter extends React.Component<Props, State> {
             src={`/assets/portrait-${characterId}.jpg`}
           />
         </div>
-        <div className="dd__modal__character__name">
-          {characterName}
-        </div>
+        <div className="dd__modal__character__name">{characterName}</div>
         <div className="dd__modal__character__bio">
-          Młoda, inteligentna, piękna. Wychowana tylko do tego, żeby w
-          trafić do pałacu jako luksusowa niewolnica, do czego
-          faktycznie ma wszystkie predyspozycje. Większość życia
-          spędziła w wygodzie, dlatego nie ma dobrej kondycji.
-
-          Nigdy nie akceptowała do końca swego losu, mimo, że w gruncie
-          rzeczy oferował komfort (i względną beztroskę) i zawsze w
+          Młoda, inteligentna, piękna. Wychowana tylko do tego, żeby w trafić do
+          pałacu jako luksusowa niewolnica, do czego faktycznie ma wszystkie
+          predyspozycje. Większość życia spędziła w wygodzie, dlatego nie ma
+          dobrej kondycji. Nigdy nie akceptowała do końca swego losu, mimo, że w
+          gruncie rzeczy oferował komfort (i względną beztroskę) i zawsze w
           miarę możliwości zakradała się do biblioteki, żeby dowiedzieć
-          się czegoś o świecie przynajmniej w ten sposób. Zawsze
-          liczyła na to, że może kiedyś będzie miała okazję wyrwać się 
-          i sama o siebie zadbać, a wtedy każda wiedza może się
-          przydać.
-
-          Podejrzanie mocno interesuje się czarnymi kotami i kurzymi
-          łapkami, a nocą podczas podróży często patrzy w gwiazdy. Moore
-          wydaje się czasami podejrzewać ją o czarną magię, ale ona
-          parska śmiechem, kiedy zauważa takie niedorzeczne sugestie.
+          się czegoś o świecie przynajmniej w ten sposób. Zawsze liczyła na to,
+          że może kiedyś będzie miała okazję wyrwać się  i sama o siebie zadbać,
+          a wtedy każda wiedza może się przydać. Podejrzanie mocno interesuje
+          się czarnymi kotami i kurzymi łapkami, a nocą podczas podróży często
+          patrzy w gwiazdy. Moore wydaje się czasami podejrzewać ją o czarną
+          magię, ale ona parska śmiechem, kiedy zauważa takie niedorzeczne
+          sugestie.
         </div>
         <dl className="dd__modal__character__stats">
-          <dt>
-            Siła
-          </dt>
+          <dt>Siła</dt>
           <dd>1/0-20</dd>
-          <dt>
-            Zręczność
-          </dt>
+          <dt>Zręczność</dt>
           <dd>1/0-20</dd>
-          <dt>
-            Kondycja
-          </dt>
+          <dt>Kondycja</dt>
           <dd>1/0-20</dd>
-          <dt>
-            Inteligencja
-          </dt>
+          <dt>Inteligencja</dt>
           <dd>1/0-20</dd>
-          <dt>
-            Wola
-          </dt>
+          <dt>Wola</dt>
           <dd>1/0-20</dd>
-          <dt>
-            Empatia
-          </dt>
+          <dt>Empatia</dt>
           <dd>1/0-20</dd>
         </dl>
         <dl className="dd__modal__character__soul">
-          <dt>
-            Nieufność i nieposłuszeństwo
-          </dt>
+          <dt>Nieufność i nieposłuszeństwo</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Skłonność do gniewu
-          </dt>
+          <dt>Skłonność do gniewu</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Pamiętliwość
-          </dt>
+          <dt>Pamiętliwość</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Oszczerstwa i pomówienia
-          </dt>
+          <dt>Oszczerstwa i pomówienia</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Gadulstwo
-          </dt>
+          <dt>Gadulstwo</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Łgarstwo
-          </dt>
+          <dt>Łgarstwo</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Przygnębienie i lenistwo
-          </dt>
+          <dt>Przygnębienie i lenistwo</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Obżarstwo
-          </dt>
+          <dt>Obżarstwo</dt>
           <dd>20/0-100</dd>
           <dt>
             Rozpusta i nieczystość
             {/*Cnotliwość i czystość cielesna*/}
           </dt>
           <dd>20/0-100</dd>
-          <dt>
-            Chciwość
-          </dt>
+          <dt>Chciwość</dt>
           <dd>20/0-100</dd>
           <dt>
             Materializm
             {/*Wolność od materializmu*/}
           </dt>
           <dd>20/0-100</dd>
-          <dt>
-            Brak poczucia rzeczywistości, niezaangażowanie i dystans
-          </dt>
+          <dt>Brak poczucia rzeczywistości, niezaangażowanie i dystans</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Bojaźliwość
-          </dt>
+          <dt>Bojaźliwość</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Próżność
-          </dt>
+          <dt>Próżność</dt>
           <dd>20/0-100</dd>
-          <dt>
-            Pycha / zawyżone poczucie własnej wartości
-          </dt>
+          <dt>Pycha / zawyżone poczucie własnej wartości</dt>
           <dd>20/0-100</dd>
           <dt>
             Podłość, matactwa i chaos
