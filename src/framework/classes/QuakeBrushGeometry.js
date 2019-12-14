4f77c8078776a447fde8a7b336cfce1ff07251a6
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2019-12-14 18:46:27 +0100

    create UVs in a separate function

--- src/framework/classes/QuakeBrushGeometry.js
+++ src/framework/classes/QuakeBrushGeometry.js
@@ -8,10 +8,57 @@ import quake2three from "../helpers/quake2three";
 import three2quake from "../helpers/three2quake";
 
 import type { ConvexHull as ConvexHullInterface } from "three/examples/jsm/math/ConvexHull";
-import type { Geometry, Texture } from "three";
+import type { Geometry, Texture, Vector2, Vector3 } from "three";
 
 import type { QuakeBrush } from "../interfaces/QuakeBrush";
 import type { QuakeBrushGeometry as QuakeBrushGeometryInterface } from "../interfaces/QuakeBrushGeometry";
+
+function generateUVs(face: Face3, textureSide: number, v1: Vector3, v2: Vector3, v3: Vector3): [Vector2, Vector2, Vector2] {
+  // prettier-ignore
+  if (face.normal.x > face.normal.y && face.normal.x > face.normal.z) {
+    return [
+      new THREE.Vector2(v1.z / textureSide, v1.y / textureSide),
+      new THREE.Vector2(v2.z / textureSide, v2.y / textureSide),
+      new THREE.Vector2(v3.z / textureSide, v3.y / textureSide)
+    ];
+  } else if (face.normal.y > face.normal.x && face.normal.y > face.normal.z) {
+    return [
+      new THREE.Vector2(v1.z / textureSide, v1.x / textureSide),
+      new THREE.Vector2(v2.z / textureSide, v2.x / textureSide),
+      new THREE.Vector2(v3.z / textureSide, v3.x / textureSide)
+    ];
+  } else if (face.normal.z > face.normal.x && face.normal.z > face.normal.y) {
+    return [
+      new THREE.Vector2(v1.x / textureSide, v1.y / textureSide),
+      new THREE.Vector2(v2.x / textureSide, v2.y / textureSide),
+      new THREE.Vector2(v3.x / textureSide, v3.y / textureSide)
+    ];
+  } else if (face.normal.x < face.normal.y && face.normal.x < face.normal.z) {
+    return [
+      new THREE.Vector2(v1.z / textureSide, v1.y / textureSide),
+      new THREE.Vector2(v2.z / textureSide, v2.y / textureSide),
+      new THREE.Vector2(v3.z / textureSide, v3.y / textureSide)
+    ];
+  } else if (face.normal.y < face.normal.x && face.normal.y < face.normal.z) {
+    return [
+      new THREE.Vector2(v1.z / textureSide, v1.x / textureSide),
+      new THREE.Vector2(v2.z / textureSide, v2.x / textureSide),
+      new THREE.Vector2(v3.z / textureSide, v3.x / textureSide)
+    ];
+  } else if (face.normal.z < face.normal.x && face.normal.z < face.normal.y) {
+    return [
+      new THREE.Vector2(v1.x / textureSide, v1.y / textureSide),
+      new THREE.Vector2(v2.x / textureSide, v2.y / textureSide),
+      new THREE.Vector2(v3.x / textureSide, v3.y / textureSide)
+    ];
+  } else {
+    return [
+      new THREE.Vector2(0, 0),
+      new THREE.Vector2(0, 0),
+      new THREE.Vector2(0, 0)
+    ];
+  }
+}
 
 export default class QuakeBrushGeometry implements QuakeBrushGeometryInterface {
   +quakeBrush: QuakeBrush;
@@ -47,11 +94,11 @@ export default class QuakeBrushGeometry implements QuakeBrushGeometryInterface {
       const v2 = geometry.vertices[i + 1];
       const v3 = geometry.vertices[i + 2];
 
-      const v1three = three2quake(v1);
-      const v2three = three2quake(v2);
-      const v3three = three2quake(v3);
-
-      const halfSpace = this.quakeBrush.getHalfSpaceByCopolarPoints(v1three, v2three, v3three);
+      const halfSpace = this.quakeBrush.getHalfSpaceByCopolarPoints(
+        three2quake(v1),
+        three2quake(v2),
+        three2quake(v3),
+      );
       const textureName = halfSpace.getTexture();
 
       const textureIndex = textures.findIndex(texture => texture.name === textureName);
@@ -59,50 +106,7 @@ export default class QuakeBrushGeometry implements QuakeBrushGeometryInterface {
       const textureSide = texture.image.naturalWidth;
       const face = new THREE.Face3(i, i + 1, i + 2, rawFace.normal, rawFace.color, textureIndex);
 
-      // prettier-ignore
-      if (face.normal.x > face.normal.y && face.normal.x > face.normal.z) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.z / textureSide, v1.y / textureSide),
-          new THREE.Vector2(v2.z / textureSide, v2.y / textureSide),
-          new THREE.Vector2(v3.z / textureSide, v3.y / textureSide)
-        ]);
-      } else if (face.normal.y > face.normal.x && face.normal.y > face.normal.z) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.z / textureSide, v1.x / textureSide),
-          new THREE.Vector2(v2.z / textureSide, v2.x / textureSide),
-          new THREE.Vector2(v3.z / textureSide, v3.x / textureSide)
-        ]);
-      } else if (face.normal.z > face.normal.x && face.normal.z > face.normal.y) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.x / textureSide, v1.y / textureSide),
-          new THREE.Vector2(v2.x / textureSide, v2.y / textureSide),
-          new THREE.Vector2(v3.x / textureSide, v3.y / textureSide)
-        ]);
-      } else if (face.normal.x < face.normal.y && face.normal.x < face.normal.z) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.z / textureSide, v1.y / textureSide),
-          new THREE.Vector2(v2.z / textureSide, v2.y / textureSide),
-          new THREE.Vector2(v3.z / textureSide, v3.y / textureSide)
-        ]);
-      } else if (face.normal.y < face.normal.x && face.normal.y < face.normal.z) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.z / textureSide, v1.x / textureSide),
-          new THREE.Vector2(v2.z / textureSide, v2.x / textureSide),
-          new THREE.Vector2(v3.z / textureSide, v3.x / textureSide)
-        ]);
-      } else if (face.normal.z < face.normal.x && face.normal.z < face.normal.y) {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(v1.x / textureSide, v1.y / textureSide),
-          new THREE.Vector2(v2.x / textureSide, v2.y / textureSide),
-          new THREE.Vector2(v3.x / textureSide, v3.y / textureSide)
-        ]);
-      } else {
-        geometry.faceVertexUvs[0].push([
-          new THREE.Vector2(0, 0),
-          new THREE.Vector2(0, 0),
-          new THREE.Vector2(0, 0)
-        ]);
-      }
+      geometry.faceVertexUvs[0].push(generateUVs(face, textureSide, v1, v2, v3));
 
       geometry.faces.push(face);
       i += 3;
