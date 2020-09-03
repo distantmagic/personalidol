c981969f159f73d7905d61c73addd484275772a4
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2020-09-03 20:52:00 +0200

    bootstrap packages before typechecking

--- Makefile_packages.mk
+++ Makefile_packages.mk
@@ -1,7 +1,7 @@
 SOURCES_TS := $(shell find src -type f -name "*.ts")
 
 .PHONY: build
-build: bootstrap node_modules/.bin/esbuild $(SOURCES_TS)
+build: node_modules/.bin/esbuild $(SOURCES_TS)
 	yarn run esbuild \
 		--outdir=lib \
 		--platform=browser \
@@ -25,11 +25,11 @@ lib:
 node_modules/.bin/jest node_modules/.bin/prettier node_modules/.bin/esbuild: node_modules
 
 .PHONY: prettier
-prettier: bootstrap node_modules/.bin/prettier $(SOURCES_TS)
+prettier: node_modules/.bin/prettier $(SOURCES_TS)
 	yarn run prettier --write --print-width 180 "{components,elements,src}/**/*.{ts,tsx}"
 
 .PHONY: test
-test: bootstrap node_modules/.bin/jest
+test: node_modules/.bin/jest
 	yarn run jest
 
 .PHONY: test.watch
@@ -37,9 +37,9 @@ test.watch: node_modules
 	yarn run jest --watch
 
 .PHONY: typecheck
-typecheck: bootstrap node_modules
+typecheck: node_modules
 	yarn run tsc --noEmit
 
 .PHONY: typecheck.watch
-typecheck.watch: bootstrap node_modules
+typecheck.watch: node_modules
 	yarn run tsc --noEmit --watch
