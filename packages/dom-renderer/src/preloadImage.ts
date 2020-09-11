f278b8baba7f46054ed2c1397fb99862b7282afc
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2020-09-11 23:15:11 +0200

    add crossorigin:anonymous flag to downloaded images

--- packages/dom-renderer/src/preloadImage.ts
+++ packages/dom-renderer/src/preloadImage.ts
@@ -2,6 +2,7 @@ export function preloadImage(textureUrl: string): Promise<HTMLImageElement> {
   return new Promise(function (resolve, reject) {
     const image = new Image();
 
+    image.crossOrigin = "Anonymous";
     image.onerror = reject;
     image.onload = function () {
       resolve(image);
--- packages/website/internal/webroute/index.go
+++ packages/website/internal/webroute/index.go
@@ -7,16 +7,15 @@ import (
 )
 
 func Index(c *gin.Context) {
-	c.Writer.Header().Add("Link", "</public/lib/worker_atlas.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_atlas.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_md2.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_offscreen.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_progress.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_quakemaps.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/lib/worker_textures.js>; rel=preload; as=worker")
-	c.Writer.Header().Add("Link", "</public/fonts/font-almendra-regular.ttf>; rel=preload; as=font; crossorigin")
-	c.Writer.Header().Add("Link", "</public/fonts/font-mukta-light.ttf>; rel=preload; as=font; crossorigin")
-	c.Writer.Header().Add("Link", "</public/fonts/font-mukta-medium.ttf>; rel=preload; as=font; crossorigin")
-
+	// c.Writer.Header().Add("Link", "</public/lib/worker_atlas.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_atlas.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_md2.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_offscreen.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_progress.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_quakemaps.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/lib/worker_textures.js>; rel=preload; as=worker")
+	// c.Writer.Header().Add("Link", "</public/fonts/font-almendra-regular.ttf>; rel=preload; as=font; crossorigin")
+	// c.Writer.Header().Add("Link", "</public/fonts/font-mukta-light.ttf>; rel=preload; as=font; crossorigin")
+	// c.Writer.Header().Add("Link", "</public/fonts/font-mukta-medium.ttf>; rel=preload; as=font; crossorigin")
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
