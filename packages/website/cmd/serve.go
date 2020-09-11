a7c2feffb0e34082bf63b23bd2ced6815eff7df1
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2020-09-11 15:55:24 +0200

    update website server cli arguments, add wait group

--- packages/website/cmd/serve.go
+++ packages/website/cmd/serve.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"sync"
+
 	"github.com/urfave/cli/v2"
 
 	"personalidol.com/website/internal/webserver"
@@ -8,7 +10,7 @@ import (
 
 var Serve = cli.Command{
 	Name:   "serve",
-	Usage:  "Starts HTTPS server and optionally HTTP server for local development",
+	Usage:  "Starts HTTPS server and optionally HTTP server.",
 	Flags:  flags,
 	Action: serve,
 }
@@ -17,34 +19,40 @@ var flags = []cli.Flag{
 	&cli.StringFlag{
 		Name:  "http-listen",
 		Usage: "HTTP listen string. If it's not provided then HTTP server won't be started. (example: \"127.0.0.1:8080\")",
+		Value: ":80",
 	},
 	&cli.StringFlag{
 		Name:  "https-listen",
 		Usage: "HTTPS listen string (example: \"example.com:443\")",
-		Value: ":443",
 	},
 	&cli.StringFlag{
 		Name:     "ssl-key",
 		Usage:    "SSL private key file name (example: \"./ssl.key\")",
-		Required: true,
 	},
 	&cli.StringFlag{
 		Name:     "ssl-cert",
 		Usage:    "SSL certificate file name (example: \"./ssl.cert\")",
-		Required: true,
 	},
 }
 
 func serve(c *cli.Context) error {
+	var wg sync.WaitGroup
+
 	router := webserver.Create()
 
 	httpListen := c.String("http-listen")
-
 	if httpListen != "" {
+		wg.Add(1)
 		go router.Run(httpListen)
 	}
 
-	router.RunTLS(c.String("https-listen"), c.String("ssl-cert"), c.String("ssl-key"))
+	httpsListen := c.String("https-listen")
+	if httpsListen != "" {
+		wg.Add(1)
+		go router.RunTLS(c.String("https-listen"), c.String("ssl-cert"), c.String("ssl-key"))
+	}
+
+	wg.Wait()
 
 	return nil
 }
