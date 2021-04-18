0ae26097b6fa2b69d87f5554d30cb5aa0522ae2c
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2021-04-18 02:20:23 +0200

    chore: update dependencies

--- packages/website/src/createI18next.ts
+++ packages/website/src/createI18next.ts
@@ -3,6 +3,7 @@ import i18next from "i18next";
 
 import { LoglevelPlugin } from "@personalidol/i18n/src/LoglevelPlugin";
 import { monitorResponseProgress } from "@personalidol/framework/src/monitorResponseProgress";
+import { noop } from "@personalidol/framework/src/noop";
 import { Progress } from "@personalidol/framework/src/Progress";
 
 import type { Logger } from "loglevel";
@@ -12,50 +13,52 @@ export function createI18next(logger: Logger, progressMessagePort: MessagePort):
   i18next
     .use(HTTPBackend)
     .use(LoglevelPlugin(logger))
-    .init({
-      debug: "debug" === __LOG_LEVEL,
-      fallbackLng: "en",
-      lng: "en",
-      ns: ["ui"],
-      defaultNS: "__NOT_USED",
-      supportedLngs: ["en", "pl"],
+    .init(
+      {
+        debug: "debug" === __LOG_LEVEL,
+        fallbackLng: "en",
+        lng: "en",
+        ns: ["ui"],
+        defaultNS: "__NOT_USED",
+        supportedLngs: ["en", "pl"],
 
-      backend: {
-        loadPath: `${__LOCALES_LOAD_PATH}?${__CACHE_BUST}`,
-        parse: JSON.parse,
-        request: function (options: any, url: string, payload: any, callback: Function) {
-          const progress = Progress(progressMessagePort, "translation", url);
+        backend: {
+          loadPath: `${__LOCALES_LOAD_PATH}?${__CACHE_BUST}`,
+          request: function (options: any, url: string, payload: any, callback: Function) {
+            const progress = Progress(progressMessagePort, "translation", url);
 
-          progress
-            .wait(
-              fetch(url)
-                .then(monitorResponseProgress(progress.progress, true))
-                .then(function (response: Response) {
-                  if (!response.ok) {
-                    return void callback(response.statusText || "Error", {
-                      status: response.status,
+            progress
+              .wait(
+                fetch(url)
+                  .then(monitorResponseProgress(progress.progress, true))
+                  .then(function (response: Response) {
+                    if (!response.ok) {
+                      return void callback(response.statusText || "Error", {
+                        status: response.status,
+                      });
+                    }
+
+                    return response.text();
+                  })
+                  .then(function (data: undefined | string) {
+                    if (!data) {
+                      return;
+                    }
+
+                    callback(null, {
+                      status: 200,
+                      data: data,
                     });
-                  }
-
-                  return response.text();
-                })
-                .then(function (data: undefined | string) {
-                  if (!data) {
-                    return;
-                  }
-
-                  callback(null, {
-                    status: 200,
-                    data: data,
-                  });
-                })
-            )
-            .catch(function (error) {
-              callback(error);
-            });
+                  })
+              )
+              .catch(function (error) {
+                callback(error);
+              });
+          },
         },
       },
-    });
+      noop
+    );
 
   return i18next;
 }
